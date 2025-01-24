715a6c2965de29e6f608f2881b5ffd6bfd7ef601
Chinwe Oti <chinwe_oti@yahoo.com>
2025-01-24 13:45:24 -0800

    end_other

---
+++ end_other.py
@@ -0,0 +1,11 @@
+
+# Given two strings, return True if either of the strings appears at the very end of the other string, 
+# ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+
+def end_other(a, b):
+  a=a.lower()
+  b=b.lower()
+  if a.endswith(b) or b.endswith(a):
+    return True
+  return False
+        
