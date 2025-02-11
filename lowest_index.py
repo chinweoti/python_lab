c223ef447ce042cea4c58fe9d25d660bacd6c9c9
Chinwe Oti <chinwe_oti@yahoo.com>
2025-02-11 11:18:12 -0800

    lowest_index

--- lowest_index.py
+++ lowest_index.py
@@ -2,7 +2,9 @@
 # Given a sorted array arr of distinct integers, write a function indexEqualsValueSearch that returns the lowest index i for which arr[i] == i. Return -1 if there is no such index. Analyze the time and space complexities of your solution and explain its correctness.
 
 #best for time complexity 
- start = 0
+def index_equals_value_search(arr: List[int]) -> int:
+
+    start = 0
     end = len(arr) - 1
 
     while (start <= end):
